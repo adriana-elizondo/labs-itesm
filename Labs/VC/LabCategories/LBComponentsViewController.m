d27130273a8e9918c757fbe0b64f6c8e65ae0682
Adriana Elizondo <adriana@gammapartners.com>
2015-07-15 08:29:34 -0600

    nav controller

--- Labs/VC/LabCategories/LBComponentsViewController.m
+++ Labs/VC/LabCategories/LBComponentsViewController.m
@@ -12,14 +12,17 @@
 #import "LBComponent.h"
 #import "RequestHelper.h"
 #import "UIImage+ImageEffects.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
-@interface LBComponentsViewController ()<UISearchResultsUpdating, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate>{
+@interface LBComponentsViewController ()<UISearchResultsUpdating, UISearchBarDelegate, UISearchControllerDelegate, UITableViewDataSource, UITableViewDelegate>{
     NSArray *components;
     LBSearchResultsTableViewController *searchResultsController;
 }
+@property CGFloat percentScrolled;
 @property (weak, nonatomic) IBOutlet UITableView *componentsTableView;
 @property (nonatomic, strong) UISearchController *searchController;
-@property (weak, nonatomic) IBOutlet UIImageView *background;
+@property (weak, nonatomic) IBOutlet UIImageView *categoryImage;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topConstraint;
 
 @end
 
@@ -31,15 +34,20 @@
     //Register class for collection view cell
     UINib *nib = [UINib nibWithNibName:@"LBComponentTableViewCell" bundle: nil];
     [self.componentsTableView registerNib:nib forCellReuseIdentifier:@"componentCell"];
+    
+    UIBarButtonItem *searchBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(startSearch)];
+    self.navigationItem.rightBarButtonItem = searchBarButton;
 
-    [self setUpSearchController];
     
     //Get components from server
     [RequestHelper getRequestWithQueryString:[NSString stringWithFormat:@"%@?id_category_fk=%@", self.componentsURL, self.idCategory] response:^(id response, id error) {
         components = [[NSArray alloc] initWithArray:[LBComponent arrayOfModelsFromDictionaries:response]];
         [self.componentsTableView reloadData];
     }];
-    // Do any additional setup after loading the view from its nib.
+    
+      [self.componentsTableView setContentInset:UIEdgeInsetsMake(80, 0, 0, 0)];
+      [self tableViewObserver];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -49,24 +57,37 @@
 
 -(void)viewWillAppear:(BOOL)animated{
     self.automaticallyAdjustsScrollViewInsets = NO;
-    //self.searchBar.hidden = YES;
 }
 
--(void)setUpSearchController{
+-(void)tableViewObserver{
+    @weakify(self)
+    [RACObserve(self, self.percentScrolled) subscribeNext:^(id percentScrolled) {
+        @strongify(self);
+        
+        CGFloat fpercentScrolled = [percentScrolled floatValue];
+        CGFloat scale = fminf(1.f, fmaxf(.4f, fpercentScrolled));
+        CGFloat top = fminf(1.f, fmaxf(.0f, fpercentScrolled));
+        
+        self.categoryImage.alpha = fpercentScrolled * 2 - 1;
+        self.categoryImage.transform = CGAffineTransformMakeScale(scale, scale);
+        self.topConstraint.constant = self.topConstraint.constant - top;
+    }];
+}
+
+#pragma mark - Start search
+- (void)startSearch{
     searchResultsController = [[LBSearchResultsTableViewController alloc] init];
     
     self.searchController = [[UISearchController alloc] initWithSearchResultsController:searchResultsController];
     self.searchController.searchResultsUpdater = self;
-    self.searchController.searchResultsUpdater = self;
-    [self.searchController.searchBar sizeToFit];
-    
-    searchResultsController.tableView.delegate = self;
-    self.searchController.dimsBackgroundDuringPresentation = NO; // default is YES
-    self.searchController.searchBar.delegate = self;
-    
-    self.componentsTableView.tableHeaderView = self.searchController.searchBar;
+    self.searchController.hidesNavigationBarDuringPresentation = NO;
+    self.searchController.dimsBackgroundDuringPresentation = NO;
     self.definesPresentationContext = YES;
+    self.searchController.delegate = self;
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+    [self presentViewController:self.searchController animated:YES completion:nil];
 }
+
 
 #pragma mark - UISearchControllerDelegate & UISearchResultsDelegate
 - (void)updateSearchResultsForSearchController:(UISearchController *)searchController
@@ -91,6 +112,10 @@
     LBSearchResultsTableViewController *tableController = (LBSearchResultsTableViewController *)self.searchController.searchResultsController;
     tableController.searchResults = searchResults;
     [tableController.tableView reloadData];
+}
+
+-(void)willDismissSearchController:(UISearchController *)searchController{
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
 }
 
 #pragma mark - UISearchBarDelegate
@@ -120,5 +145,12 @@
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     
 }
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    
+    self.percentScrolled =  scrollView.contentOffset.y;
+}
+
 
 @end
