bfc16755a4c921744a12257e9d44f7a807d7ad23
Adriana Elizondo <adriana@gammapartners.com>
2015-07-16 08:24:57 -0600

    Background animation

--- Labs/VC/LabCategories/LBComponentsViewController.m
+++ Labs/VC/LabCategories/LBComponentsViewController.m
@@ -13,6 +13,7 @@
 #import "RequestHelper.h"
 #import "UIImage+ImageEffects.h"
 #import <ReactiveCocoa/ReactiveCocoa.h>
+#import <pop/POP.h>
 
 @interface LBComponentsViewController ()<UISearchResultsUpdating, UISearchBarDelegate, UISearchControllerDelegate, UITableViewDataSource, UITableViewDelegate>{
     NSArray *components;
@@ -22,7 +23,6 @@
 @property (weak, nonatomic) IBOutlet UITableView *componentsTableView;
 @property (nonatomic, strong) UISearchController *searchController;
 @property (weak, nonatomic) IBOutlet UIImageView *categoryImage;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topConstraint;
 
 @end
 
@@ -38,15 +38,15 @@
     UIBarButtonItem *searchBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(startSearch)];
     self.navigationItem.rightBarButtonItem = searchBarButton;
 
+    self.componentsTableView.backgroundColor = [UIColor colorWithWhite:1.0 alpha:.7];
     
     //Get components from server
     [RequestHelper getRequestWithQueryString:[NSString stringWithFormat:@"%@?id_category_fk=%@", self.componentsURL, self.idCategory] response:^(id response, id error) {
         components = [[NSArray alloc] initWithArray:[LBComponent arrayOfModelsFromDictionaries:response]];
         [self.componentsTableView reloadData];
+        [self startAnimation:NO repeatCount:0];
     }];
     
-      [self.componentsTableView setContentInset:UIEdgeInsetsMake(80, 0, 0, 0)];
-      [self tableViewObserver];
 
 }
 
@@ -57,22 +57,38 @@
 
 -(void)viewWillAppear:(BOOL)animated{
     self.automaticallyAdjustsScrollViewInsets = NO;
+    self.view.backgroundColor = [self pixelColorInImage:self.categoryImage.image atX:0 atY:0];
+    [self startAnimation:YES repeatCount:HUGE_VALF];
 }
 
--(void)tableViewObserver{
-    @weakify(self)
-    [RACObserve(self, self.percentScrolled) subscribeNext:^(id percentScrolled) {
-        @strongify(self);
-        
-        CGFloat fpercentScrolled = [percentScrolled floatValue];
-        CGFloat scale = fminf(1.f, fmaxf(.4f, fpercentScrolled));
-        CGFloat top = fminf(1.f, fmaxf(.0f, fpercentScrolled));
-        
-        self.categoryImage.alpha = fpercentScrolled * 2 - 1;
-        self.categoryImage.transform = CGAffineTransformMakeScale(scale, scale);
-        self.topConstraint.constant = self.topConstraint.constant - top;
-    }];
+-(void)startAnimation:(BOOL)reverses repeatCount:(int)count{
+    POPSpringAnimation *scaleDownAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleDownAnimation.springBounciness = 15;
+    scaleDownAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(2,2)];
+    scaleDownAnimation.autoreverses = reverses;
+    scaleDownAnimation.repeatCount= count;
+    [self.categoryImage.layer pop_addAnimation:scaleDownAnimation forKey:@"scaleDown"];
 }
+
+- (UIColor*)pixelColorInImage:(UIImage*)image atX:(int)x atY:(int)y {
+    
+    CFDataRef pixelData = CGDataProviderCopyData(CGImageGetDataProvider(image.CGImage));
+    const UInt8* data = CFDataGetBytePtr(pixelData);
+    
+    int pixelInfo = ((image.size.width  * y) + x ) * 4; // 4 bytes per pixel
+    
+    UInt8 red   = data[pixelInfo + 0];
+    UInt8 green = data[pixelInfo + 1];
+    UInt8 blue  = data[pixelInfo + 2];
+    UInt8 alpha = data[pixelInfo + 3];
+    CFRelease(pixelData);
+    
+    return [UIColor colorWithRed:red/255.0f
+                           green:green/255.0f
+                            blue:blue/255.0f
+                           alpha:alpha/255.0f];
+}
+
 
 #pragma mark - Start search
 - (void)startSearch{
@@ -137,8 +153,12 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     LBComponentTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"componentCell"];
     LBComponent *component = [components objectAtIndex:indexPath.row];
-    cell.textLabel.text = component.name;
+    cell.name.text = component.name;
     return cell;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 60;
 }
 
 #pragma mark - Table view delegate
--- Labs/VC/LabCategories/TableViewCell/LBComponentTableViewCell.h
+++ Labs/VC/LabCategories/TableViewCell/LBComponentTableViewCell.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface LBComponentTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *name;
 
 @end
